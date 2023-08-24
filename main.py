a6fa2e78334722a5d9850bf77034bfae87b5c65e
Takács Martin <originalmartin97@gmail.com>
2023-08-24 12:53:07 +0000

    refactored, functioning, snake_cased

--- main.py
+++ main.py
@@ -3,31 +3,38 @@ import smtplib
 import os
 import time
 
-UserName = "originalmartin97@gmail.com"
-UserPassword = "zklybkojdcacniof"
-IOFilePathInsurance = (
+user_name = "originalmartin97@gmail.com"
+user_password = "zklybkojdcacniof"
+io_file_path_insurance = (
     "/home/martin/projects/mailing_list_insurance/potential_customers_insurance.csv"
 )
-IOFilePathCredit = (
+io_file_path_credit = (
     "/home/martin/projects/mailing_list_insurance/potential_customers_credit.csv"
 )
-IOFilePathPension = (
+io_file_path_pension = (
     "/home/martin/projects/mailing_list_insurance/potential_customers_pension.csv"
 )
-
+template_text_path_insurance = (
+    "/home/martin/projects/mailing_list_insurance/insurance_template.txt"
+)
+template_text_path_credit = (
+    "/home/martin/projects/mailing_list_insurance/credit_template.txt"
+)
+template_text_path_pension = (
+    "/home/martin/projects/mailing_list_insurance/pension_template.txt"
+)
 
 # This code only uses csv and txt formatted files as source.
-
 
 # Sends an email to the specified address with the specified name and content as message.
 
 
-def send_email(recipients, content):
-    print("\n\nSmtp gmail server login started:")
+def send_email(to_address, content):
+    print("\n\nSMTP gmail server login started:")
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    print("Logging in as", UserName)
-    server.login(UserName, UserPassword)
+    print("Logging in as", user_name)
+    server.login(user_name, user_password)
     print("Success")
 
     print("\nCreating message:")
@@ -36,196 +43,78 @@ def send_email(recipients, content):
     message = message.encode("utf-8")
     print("Success")
 
-    # Join the recipients as a comma-separated string for the "To" field
-    to_address = ", ".join(recipients)
-
-    print("\nSending email as", UserName, "to", to_address)
-    server.sendmail(UserName, to_address, message)
+    print("\nSending email as", user_name, "to", to_address)
+    server.sendmail(user_name, to_address, message)
     print("Email sent successfully to", to_address)
 
     server.quit()
-    print("Logging out and exiting smtp gmail server")
+    print("Logging out and exiting SMTP gmail server")
+    # Sleep for one second - avoiding losing connection and errors with SMTP
     time.sleep(1)
     ("Done")
 
 
-def sendMailInsurance():
-    recipients_and_names = []
+def process(category):
+    if category == "1":
+        template_text = template_text_path_insurance
+        io_file = io_file_path_insurance
+    elif category == "2":
+        template_text = template_text_path_credit
+        io_file = io_file_path_credit
+    elif category == "3":
+        template_text = template_text_path_pension
+        io_file = io_file_path_pension
+    else:
+        print("Category is not valid")
 
-    # Reads in the template content from file
-    with open(
-        "/home/martin/projects/mailing_list_insurance/insurance_template.txt", "r"
-    ) as template:
+    with open(template_text, "r") as template:
         template_content = template.read()
 
-    # Checks if file exists
-    if os.path.isfile(IOFilePathInsurance):
-        # Opens the source file for reading and writing
-        with open(IOFilePathInsurance, "r+") as IOFile:
-            # Initiates variable for containing the rows of the source file
+    if os.path.isfile(io_file):
+        with open(io_file, "r+") as source_file:
             lines = []
+            reader = csv.reader(source_file, delimiter=",")
 
-            reader = csv.reader(IOFile, delimiter=",")
-
-            # Iterates through rows of the source file
             for row in reader:
-                # Checks if the email hasn't already been sent out to the customer
                 if row[2] != "Sent":
-                    email = row[1]
+                    address = row[1]
                     name = row[0]
+                    content = template_content.format(name)
 
-                    # Append recipient and name as a tuple
-                    recipients_and_names.append((email, name))
+                    send_email(address, content)
 
-                # Appends the current row to the lines container
+                    row[2] = "Sent"
+
                 lines.append(row)
 
-            IOFile.close()
-
+        source_file.close()
         template.close()
+        os.remove(io_file)
 
-        # Send emails to all recipients with their personalized content
-        for email, name in recipients_and_names:
-            # Create personalized content for each email
-            content = template_content.format(name)
-
-            # Send the email with personalized content
-            send_email([email], content)
-
-        # Deletes the source file beforehand
-        os.remove(IOFilePathInsurance)
-
-        # Reopen the source file to update "Sent" flag
-        with open(IOFilePathInsurance, "w", newline="") as newCsvFile:
-            writer = csv.writer(newCsvFile, delimiter=",")
+        with open(io_file, "w", newline="") as new_csv_file:
+            writer = csv.writer(new_csv_file, delimiter=",")
 
             # Write the rows to the source file using writerows()
             writer.writerows(lines)
-            newCsvFile.close()
+            new_csv_file.close()
 
-
-def sendMailCredit():
-    recipients_and_names = []
-
-    # Reads in the template content from file
-    with open(
-        "/home/martin/projects/mailing_list_insurance/credit_template.txt", "r"
-    ) as template:
-        template_content = template.read()
-
-    # Checks if file exists
-    if os.path.isfile(IOFilePathCredit):
-        # Opens the source file for reading and writing
-        with open(IOFilePathCredit, "r+") as IOFile:
-            # Initiates variable for containing the rows of the source file
-            lines = []
-
-            reader = csv.reader(IOFile, delimiter=",")
-
-            # Iterates through rows of the source file
-            for row in reader:
-                # Checks if the email hasn't already been sent out to the customer
-                if row[2] != "Sent":
-                    email = row[1]
-                    name = row[0]
-
-                    # Append recipient and name as a tuple
-                    recipients_and_names.append((email, name))
-
-                # Appends the current row to the lines container
-                lines.append(row)
-
-            IOFile.close()
-
-        template.close()
-
-        # Send emails to all recipients with their personalized content
-        for email, name in recipients_and_names:
-            # Create personalized content for each email
-            content = template_content.format(name)
-
-            # Send the email with personalized content
-            send_email([email], content)
-
-        # Deletes the source file beforehand
-        os.remove(IOFilePathCredit)
-
-        # Reopen the source file to update "Sent" flag
-        with open(IOFilePathCredit, "w", newline="") as newCsvFile:
-            writer = csv.writer(newCsvFile, delimiter=",")
-
-            # Write the rows to the source file using writerows()
-            writer.writerows(lines)
-            newCsvFile.close()
-
-
-def sendMailPension():
-    recipients_and_names = []
-
-    # Reads in the template content from file
-    with open(
-        "/home/martin/projects/mailing_list_insurance/pension_template.txt", "r"
-    ) as template:
-        template_content = template.read()
-
-    # Checks if file exists
-    if os.path.isfile(IOFilePathPension):
-        # Opens the source file for reading and writing
-        with open(IOFilePathPension, "r+") as IOFile:
-            # Initiates variable for containing the rows of the source file
-            lines = []
-
-            reader = csv.reader(IOFile, delimiter=",")
-
-            # Iterates through rows of the source file
-            for row in reader:
-                # Checks if the email hasn't already been sent out to the customer
-                if row[2] != "Sent":
-                    email = row[1]
-                    name = row[0]
-
-                    # Append recipient and name as a tuple
-                    recipients_and_names.append((email, name))
-
-                # Appends the current row to the lines container
-                lines.append(row)
-
-            IOFile.close()
-
-        template.close()
-
-        # Send emails to all recipients with their personalized content
-        for email, name in recipients_and_names:
-            # Create personalized content for each email
-            content = template_content.format(name)
-
-            # Send the email with personalized content
-            send_email([email], content)
-
-        # Deletes the source file beforehand
-        os.remove(IOFilePathPension)
-
-        # Reopen the source file to update "Sent" flag
-        with open(IOFilePathPension, "w", newline="") as newCsvFile:
-            writer = csv.writer(newCsvFile, delimiter=",")
-
-            # Write the rows to the source file using writerows()
-            writer.writerows(lines)
-            newCsvFile.close()
+        lines = []
+    else:
+        print("There isn't any new potential customer file.")
 
 
 if __name__ == "__main__":
     print("Martin Marketing Mailing System")
 
     while True:
-        userInput = input(
+        user_input = input(
             "\n\n\nWhat do you want to do?\n\n0 - Exit program\n1 - Send out emails about insurance to potential customers\n2 - Send out emails about credit to potential customers\n"
         )
 
-        if userInput == "1":
-            sendMailInsurance()
-        elif userInput == "2":
-            sendMailCredit()
-        elif userInput == "0":
+        if user_input == "1":
+            process(user_input)
+        elif user_input == "2":
+            process(user_input)
+        elif user_input == "0":
             print("\n\nGoodbye! :)")
-            quit()
+            break
